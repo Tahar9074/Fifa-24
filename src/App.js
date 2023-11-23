b202328d8617016ec1101822fe68341cfc47d557
Tahar <taherchibane842@gmail.com>
2023-11-23 09:54:13 +0100

    13th commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PlayerList from './PlayersList';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <PlayerList />
+    </div>
+  )
+}
+
+export default App;
